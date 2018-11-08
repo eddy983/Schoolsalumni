3b40173b79f3673c3dc1e271232337aeb0a6d2df
eddy983 <edidiongisong7@gmail.com>
2018-11-08 15:01:14 +0100

    Changed some js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,32 @@
+//? For Smooth Scrolling Feature
+$(document).ready(function() {
+  // Add smooth scrolling to all links
+  $("a").on("click", function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
+
+
 /*  Navigation */
 var open = false;
 
@@ -284,30 +313,3 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
-//? For Smooth Scrolling Feature
-$(document).ready(function() {
-  // Add smooth scrolling to all links
-  $("a").on("click", function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top
-        },
-        800,
-        function() {
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    } // End if
-  });
-});
